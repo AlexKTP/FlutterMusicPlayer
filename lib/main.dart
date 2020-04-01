c004c0ea5dc2091c342efd048d9bf1383c5db3ec
alexKTP <alexandre.malachin@gmail.com>
2020-04-01 15:39:45 +0200

    logic implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:audioplayer/audioplayer.dart';
 import 'package:flutter/material.dart';
 
 import 'musique.dart';
@@ -36,13 +39,20 @@ class _MyHomePageState extends State<MyHomePage> {
         'https://codabee.com/wp-content/uploads/2018/06/deux.mp3')
   ];
 
+  AudioPlayer audioPlayer;
+  StreamSubscription positionSub;
+  StreamSubscription stateSub;
   Musique maMusiqueActuelle;
-  double position = 0.0;
+  Duration position = new Duration(seconds: 0);
+  Duration duree = new Duration(seconds: 0);
+  PlayerState statut = PlayerState.stopped;
+  int index = 0;
 
   @override
   void initState() {
     super.initState();
-    maMusiqueActuelle = maListeDeMusique[0];
+    maMusiqueActuelle = maListeDeMusique[index];
+    configurationAudioPlayer();
   }
 
   @override
@@ -74,25 +84,34 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 bouton(Icons.fast_rewind, 30.0, ActionMusic.rewind),
-              bouton(Icons.play_arrow,45.0, ActionMusic.play),
+                bouton(
+                    (statut == PlayerState.playing)
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                    45.0,
+                    (statut == PlayerState.playing)
+                        ? ActionMusic.pause
+                        : ActionMusic.play),
                 bouton(Icons.fast_forward, 30.0, ActionMusic.forward)
               ],
             ),
             new Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                textAvecStyle("0:0", 0.8),
-                textAvecStyle("0:22", 0.8)
+                textAvecStyle(fromDuration(position), 0.8),
+                textAvecStyle(fromDuration(duree), 0.8)
               ],
             ),
-            new Slider(value: position,
+            new Slider(
+                value: position.inSeconds.toDouble(),
                 min: 0.0,
-                max: 30.0,inactiveColor: Colors.white,
+                max: 30.0,
+                inactiveColor: Colors.white,
                 activeColor: Colors.red,
-                onChanged: (double d){
-              setState(() {
-                position = d;
-              });
+                onChanged: (double d) {
+                  setState(() {
+                    audioPlayer.seek(d);
+                  });
                 })
           ],
         ),
@@ -102,22 +121,22 @@ class _MyHomePageState extends State<MyHomePage> {
 
   IconButton bouton(IconData icone, double taille, ActionMusic action) {
     return new IconButton(
-      iconSize: taille,
+        iconSize: taille,
         color: Colors.white,
         icon: new Icon(icone),
         onPressed: () {
           switch (action) {
             case ActionMusic.play:
-              print('Play');
+              play();
               break;
             case ActionMusic.pause:
-              print('Pause');
+              pause();
               break;
             case ActionMusic.rewind:
-              print('Rewind');
+              rewind();
               break;
             case ActionMusic.forward:
-              print('Forward');
+              forward();
               break;
           }
         });
@@ -131,6 +150,78 @@ class _MyHomePageState extends State<MyHomePage> {
           color: Colors.white, fontSize: 20.0, fontStyle: FontStyle.italic),
     );
   }
+
+  void configurationAudioPlayer() {
+    audioPlayer = new AudioPlayer();
+    positionSub = audioPlayer.onAudioPositionChanged
+        .listen((pos) => setState(() => position = pos));
+    stateSub = audioPlayer.onPlayerStateChanged.listen((state) {
+      if (state == AudioPlayerState.PLAYING) {
+        setState(() {
+          duree = audioPlayer.duration;
+        });
+      } else if (state == AudioPlayerState.STOPPED) {
+        setState(() {
+          statut = PlayerState.stopped;
+        });
+      }
+    }, onError: (message) {
+      print('erreur : $message');
+      setState(() {
+        statut = PlayerState.stopped;
+        duree = new Duration(seconds: 0);
+        position = new Duration(seconds: 0);
+      });
+    });
+  }
+
+  Future play() async {
+    await audioPlayer.play(maMusiqueActuelle.urlSong);
+    setState(() {
+      statut = PlayerState.playing;
+    });
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState(() {
+      statut = PlayerState.paused;
+    });
+  }
+
+  void forward() {
+    if (index == maListeDeMusique.length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    maMusiqueActuelle = maListeDeMusique[index];
+    audioPlayer.stop();
+    configurationAudioPlayer();
+    play();
+  }
+
+  void rewind(){
+    if (position > Duration(seconds: 3)){
+      audioPlayer.seek(0.0);
+    } else {
+      if (index == 0){
+        index = maListeDeMusique.length-1;
+      } else {
+        index--;
+      }
+      maMusiqueActuelle = maListeDeMusique[index];
+      audioPlayer.stop();
+      configurationAudioPlayer();
+      play();
+    }
+  }
+
+  String fromDuration(Duration duree){
+    print(duree);
+    return duree.toString().split('.').first;
+  }
 }
 
 enum ActionMusic { play, pause, rewind, forward }
+enum PlayerState { playing, stopped, paused }
